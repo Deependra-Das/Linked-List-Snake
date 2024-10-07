b1213737a4ed81acbc47696e6fe2f91b1fa35c43
Deependra-Das <iamdeep75@gmail.com>
2024-10-07 21:56:37 +0530

    Added Level MVC ,Data, Number & RectangleShapeView

---
+++ Linked-List-Snake/include/Level/LevelController.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Level
+{
+	class LevelController
+	{
+	private:
+	public:
+		LevelController();
+		~LevelController();
+
+		void initialize();
+		void update();
+		void render();
+	};
+}
---
+++ Linked-List-Snake/source/Level/LevelModel.cpp
@@ -0,0 +1,29 @@
+#include "Level/LevelModel.h"
+
+namespace Level
+{
+	LevelModel::LevelModel()
+	{
+
+	}
+
+	LevelModel::~LevelModel()
+	{
+
+	}
+
+	void LevelModel::initialize()
+	{
+
+	}
+
+	void LevelModel::update()
+	{
+
+	}
+
+	void LevelModel::render()
+	{
+
+	}
+}
