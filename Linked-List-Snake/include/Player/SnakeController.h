f13224bbff3b41b3bc5346cf6f3d88138cf95f65
Deependra-Das <iamdeep75@gmail.com>
2024-10-09 00:09:32 +0530

    Added PlayerService & SnakeController

---
+++ Linked-List-Snake/include/Player/SnakeController.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Player
+{
+	class SnakeController
+	{
+	private:
+		void destroy();
+	public:
+		SnakeController();
+		~SnakeController();
+
+		void initialize();
+		void update();
+		void render();
+	};
+}
