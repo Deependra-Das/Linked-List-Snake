bb3082036aa7d86acffc6eb47afe5b90e485dd72
Deependra-Das <iamdeep75@gmail.com>
2024-10-09 23:14:14 +0530

    Added Single Linked List & updated Node

---
+++ Linked-List-Snake/include/LinkedList/SingleLinkedList.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "LinkedList/Node.h"
+
+namespace LinkedList
+{
+	class SingleLinkedList
+	{
+	private:
+		Node* head_node;
+		float node_width;
+		float node_height;
+		sf::Vector2i default_position;
+		Player::Direction default_direction;
+
+		Node* createNode();
+
+	public:
+		SingleLinkedList();
+		~SingleLinkedList();
+
+		void initialize(float width, float height, sf::Vector2i position, Player::Direction direction);
+		void render();
+
+	};
+}
--- Linked-List-Snake/include/Player/Node.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "BodyPart.h"
-
-struct Node
-{
-	Player::BodyPart body_part;
-	Node* next = nullptr;
-};
