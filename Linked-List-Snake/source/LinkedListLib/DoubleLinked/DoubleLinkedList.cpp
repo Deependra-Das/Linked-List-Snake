47b1f763e736f0aa3c597d233bebba58166047e6
Deependra-Das <iamdeep75@gmail.com>
2024-10-23 00:38:33 +0530

    Added Double Linked List

---
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -0,0 +1,104 @@
+#include "LinkedListLib/DoubleLinked/DoubleLinkedList.h"
+#include "LinkedListLib/DoubleLinked/DoubleNode.h"
+#include "Level/LevelView.h"
+
+namespace LinkedListLib
+{
+    namespace DoubleLinked
+    {
+        DoubleLinkedList::DoubleLinkedList() = default;
+
+        DoubleLinkedList::~DoubleLinkedList() = default;
+
+        Node* createNode()
+        {
+            return new DoubleNode();
+        }
+
+        void insertNodeAtTail()
+        {
+
+        }
+
+        void insertNodeAtHead()
+        {
+
+        }
+
+        void insertNodeAtMiddle()
+        {
+
+        }
+
+        void insertNodeAtIndex(int index)
+        {
+
+        }
+
+        void shiftNodesAfterInsertion(Node* new_node, Node* cur_node, Node* prev_node)
+        {
+
+        }
+
+
+        void removeNodeAtTail()
+        {
+
+        }
+
+        void removeNodeAtHead()
+        {
+
+        }
+
+        void removeNodeAtMiddle()
+        {
+
+        }
+
+        void removeNodeAt(int index)
+        {
+
+        }
+
+        void removeNodeAtIndex(int index)
+        {
+
+        }
+
+        void removeAllNodes()
+        {
+
+        }
+
+        void removeHalfNodes()
+        {
+
+        }
+
+        void shiftNodesAfterRemoval(Node* cur_node)
+        {
+
+        }
+
+        Direction reverse()
+        {
+
+        }
+
+        void DoubleLinkedList::shiftNodesAfterInsertion(Node* new_node, Node* cur_node, Node* prev_node)
+        {
+
+        }
+
+        void DoubleLinkedList::removeNodeAtIndex(int index)
+        {
+
+        }
+
+        void DoubleLinkedList::shiftNodesAfterRemoval(Node* cur_node)
+        {
+
+        }
+    }
+}
